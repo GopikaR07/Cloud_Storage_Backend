ebbd8922408d6c6ddfc34cf67d811c801be88fa6
GopikaR07 <gopikasg07@gmail.com>
2026-08-26 00:00:56 +0530

    Day 2

--- src/app.js
+++ src/app.js
@@ -4,8 +4,12 @@ const pool = require("./config/db");
 
 const app = express();
 
+const authRoutes = require("./routes/authRoutes");
+
+
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/health", (req, res) => {
     res.json({
